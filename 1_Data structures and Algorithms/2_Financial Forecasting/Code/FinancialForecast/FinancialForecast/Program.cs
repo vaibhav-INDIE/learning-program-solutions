501651480a81619e5b7a4f3e8da91c2caa0e7046
Vaibhav Jain <vaibhavtarun52@gmail.com>
2025-06-21 09:39:44 +0530

    Completed all week 1 solutions

---
+++ 1_Data structures and Algorithms/2_Financial Forecasting/Code/FinancialForecast/FinancialForecast/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+
+
+    static double PredictFutureValue(double initialAmount, double rate, int years)
+    {
+
+        if (years == 0)
+            return initialAmount;
+
+        return PredictFutureValue(initialAmount, rate, years - 1) * (1 + rate);
+    }
+
+    static void Main()
+    {
+        double initial = 1000;
+        double rate = 0.05; // assuming 5% annual growth
+        int years = 5;
+
+        double futureValue = PredictFutureValue(initial, rate, years);
+        Console.WriteLine($"Future Value after {years} years: Rs{futureValue:F2}");
+    }
+}
